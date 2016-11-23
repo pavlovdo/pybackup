9cfaa9dbf7f616fa2d205ec0c2f7312582d519d8
Denis O. Pavlov <pavlovdo@gmail.com>
2016-11-23 13:14:28 +0300

    fixed small bug in dockercheck.sh

--- dockercheck.sh
+++ dockercheck.sh
@@ -9,7 +9,7 @@ then
 	sudo docker stop $(sudo docker ps | grep $PROJECT | awk '{print substr($1,1)}')	
 	sudo docker load -i $PRODUCTION_DIR/$PROJECT/docker-ubuntu-$PROJECT.img
 	if $HOST_MOUNT_DIR
-		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data ubuntu:$PROJECT -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR
+		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR ubuntu:$PROJECT
 	else
 		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data ubuntu:$PROJECT
 	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
