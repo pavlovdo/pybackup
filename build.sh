7fb73ff640436407dab24331542c8b1438cb509c
Denis O. Pavlov <pavlovdo@gmail.com>
2016-11-23 12:08:18 +0300

    add command in build.sh for setting execution permission on scripts dockercheck.sh and outputsend.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ sudo chown jenkins:jenkins ubuntu-$PROJECT-$BUILD_NUMBER.img
 scp ubuntu-$PROJECT-$BUILD_NUMBER.img jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker-ubuntu-$PROJECT.img
 scp dockercheck.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockercheck.sh
 scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/outputsend.sh
+ssh jenkins@$PRODUCTION_SERVER "chmod u+x $PRODUCTION_DIR/$PROJECT/dockercheck.sh"
+ssh jenkins@$PRODUCTION_SERVER "chmod u+x $PRODUCTION_DIR/$PROJECT/outputsend.sh"
 ssh jenkins@$PRODUCTION_SERVER "echo '*/5 * * * *	$PRODUCTION_DIR/$PROJECT/dockercheck.sh' > /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "echo '30 05 * * *       $PRODUCTION_DIR/$PROJECT/outputsend.sh' >> /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "crontab /tmp/crontab"
