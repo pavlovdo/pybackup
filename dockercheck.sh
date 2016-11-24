5b77a7836d086bedb82a3fcc15c5e7766c12ce7e
Denis O. Pavlov <pavlovdo@gmail.com>
2016-11-24 19:19:17 +0300

    added printing of filename and modofoation time in subdirectories

--- dockercheck.sh
+++ dockercheck.sh
@@ -11,13 +11,13 @@ then
 	sudo docker stop $PROJECTCONTAINERS
 	echo -e '\nLoading of new image ubuntu:'$PROJECT 'from '$PRODUCTION_DIR'/'$PROJECT'/docker-ubuntu-'$PROJECT'.img'
 	sudo docker load -i $PRODUCTION_DIR/$PROJECT/docker-ubuntu-$PROJECT.img
-	if [ -n $HOST_MOUNT_DIR ]
+	if [ -z $HOST_MOUNT_DIR ]
 	then
-		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR
-		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR ubuntu:$PROJECT
-	else
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data'
 		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data ubuntu:$PROJECT
+	else
+		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR
+		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR ubuntu:$PROJECT
 	fi
 	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
 fi
--- pybackup.py
+++ pybackup.py
@@ -3,12 +3,16 @@
 from configread import configread
 # from datetime import datetime
 import os
+import time
 
 # Read the configuration file with parameters,
 # location of configuration file - as in production system
 backup_parameters = configread('/usr/local/orbit/pybackup/pybackup.conf',
                                'Backup', 'backup_main_dir')
 
-for entry in os.scandir(backup_parameters['backup_main_dir']):
-    if not entry.name.startswith('.') and entry.is_dir():
-        print(entry.name)
+for entry1 in os.scandir(backup_parameters['backup_main_dir']):
+    if entry1.is_dir():
+        for entry2 in os.scandir(entry1):
+            if entry2.is_file():
+                print(entry1.name, entry2.name,
+                      int((time.time() - os.stat(entry2.path).st_mtime)/3600))
