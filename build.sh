b0a4023e8a68f0ce5c4a2fb902e4520912b2154f
Denis O. Pavlov <pavlovdo@gmail.com>
2016-11-24 13:35:42 +0300

    added creating and modifying permissions on project directory on production server

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 PROJECT=`basename $WORKSPACE`
-PRODUCTION_SERVER=backup2.forum.lo
+PRODUCTION_SERVER=backup3.forum.lo
 PRODUCTION_DIR=/usr/local/orbit
 
 sudo docker build -t ubuntu:$PROJECT .
 sudo docker save -o ubuntu-$PROJECT-$BUILD_NUMBER.img ubuntu:$PROJECT
 sudo chown jenkins:jenkins ubuntu-$PROJECT-$BUILD_NUMBER.img
+ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT"
+ssh jenkins@$PRODUCTION_SERVER "sudo chown -vR jenkins:jenkins $PRODUCTION_DIR"
 scp ubuntu-$PROJECT-$BUILD_NUMBER.img jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker-ubuntu-$PROJECT.img
 scp dockercheck.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockercheck.sh
 scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/outputsend.sh
-ssh jenkins@$PRODUCTION_SERVER "chmod u+x $PRODUCTION_DIR/$PROJECT/dockercheck.sh"
-ssh jenkins@$PRODUCTION_SERVER "chmod u+x $PRODUCTION_DIR/$PROJECT/outputsend.sh"
+ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/dockercheck.sh"
+ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/outputsend.sh"
 ssh jenkins@$PRODUCTION_SERVER "echo '*/5 * * * *	$PRODUCTION_DIR/$PROJECT/dockercheck.sh' > /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "echo '30 05 * * *       $PRODUCTION_DIR/$PROJECT/outputsend.sh' >> /tmp/crontab"
 ssh jenkins@$PRODUCTION_SERVER "crontab /tmp/crontab"
--- dockercheck.sh
+++ dockercheck.sh
@@ -6,12 +6,18 @@ HOST_MOUNT_DIR=/backup
 
 if [ -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage ]
 then
-	sudo docker stop $(sudo docker ps | grep $PROJECT | awk '{print substr($1,1)}')	
+	PROJECTCONTAINERS=`sudo docker ps | grep $PROJECT | awk '{print substr($1,1)}'`
+	echo 'Stopping containers '$PROJECTCONTAINERS ' of project '$PROJECT
+	sudo docker stop $PROJECTCONTAINERS
+	echo -e '\nLoading of new image ubuntu:'$PROJECT 'from '$PRODUCTION_DIR'/'$PROJECT'/docker-ubuntu-'$PROJECT'.img'
 	sudo docker load -i $PRODUCTION_DIR/$PROJECT/docker-ubuntu-$PROJECT.img
 	if $HOST_MOUNT_DIR
 	then
+		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR
 		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR ubuntu:$PROJECT
 	else
+		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data'
 		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data ubuntu:$PROJECT
+	fi
 	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
 fi
