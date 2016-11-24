af86ec2975d0f7ace11bdf8c2cd62998544b27de
Denis O. Pavlov <pavlovdo@gmail.com>
2016-11-24 14:40:19 +0300

    fixed bug in dockercheck.sh

--- dockercheck.sh
+++ dockercheck.sh
@@ -7,11 +7,11 @@ HOST_MOUNT_DIR=/backup
 if [ -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage ]
 then
 	PROJECTCONTAINERS=`sudo docker ps --filter ancestor=ubuntu:$PROJECT --format "table {{.ID}}" | sed '1,1d'`
-	echo 'Stopping containers '$PROJECTCONTAINERS ' of project '$PROJECT
+	echo 'Stopping project '$PROJECT' containers '
 	sudo docker stop $PROJECTCONTAINERS
 	echo -e '\nLoading of new image ubuntu:'$PROJECT 'from '$PRODUCTION_DIR'/'$PROJECT'/docker-ubuntu-'$PROJECT'.img'
 	sudo docker load -i $PRODUCTION_DIR/$PROJECT/docker-ubuntu-$PROJECT.img
-	if $HOST_MOUNT_DIR
+	if [ -n $HOST_MOUNT_DIR ]
 	then
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR
 		sudo docker run -d -t -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR ubuntu:$PROJECT
