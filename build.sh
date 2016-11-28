a4933fbba20407742019b774c361b5219366ca2d
Denis O. Pavlov <pavlovdo@gmail.com>
2016-11-28 19:09:55 +0300

    added copying all files neccessary for docker build

--- build.sh
+++ build.sh
@@ -5,9 +5,13 @@ PRODUCTION_SERVER=backup3.forum.lo
 PRODUCTION_DIR=/usr/local/orbit
 
 ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/data ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/data"
+ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/docker ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/docker"
 ssh jenkins@$PRODUCTION_SERVER "sudo chown -vR jenkins:jenkins $PRODUCTION_DIR"
-scp Dockerfile jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/Dockerfile
-ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT $PRODUCTION_DIR/$PROJECT"
+scp Dockerfile jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/Dockerfile
+scp configread.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/configread.py
+scp pybackup.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/pybackup.py
+scp pybackup.conf jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/pybackup.conf
+ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT $PRODUCTION_DIR/$PROJECT/docker"
 scp dockercheck.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockercheck.sh
 scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/outputsend.sh
 ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/dockercheck.sh"
