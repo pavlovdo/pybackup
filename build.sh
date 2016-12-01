22dbec9e50b4384b39df6e20622cdd6eb2423440
Denis O. Pavlov <pavlovdo@gmail.com>
2016-12-01 12:12:57 +0300

    small changed in build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ PRODUCTION_DIR=/usr/local/orbit
 ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/data ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/data"
 ssh jenkins@$PRODUCTION_SERVER "sudo [ -d $PRODUCTION_DIR/$PROJECT/docker ] || sudo mkdir -pv $PRODUCTION_DIR/$PROJECT/docker"
 ssh jenkins@$PRODUCTION_SERVER "sudo chown -vR jenkins:jenkins $PRODUCTION_DIR"
-scp Dockerfile jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/Dockerfile
-scp configread.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/configread.py
-scp pybackup.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/pybackup.py
-scp pybackup.conf jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker/pybackup.conf
+scp Dockerfile jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker
+scp configread.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker
+scp $PROJECT.py jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker
+scp $PROJECT.conf jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/docker
 ssh jenkins@$PRODUCTION_SERVER "sudo docker build -t ubuntu:$PROJECT $PRODUCTION_DIR/$PROJECT/docker"
-scp dockercheck.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/dockercheck.sh
-scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT/outputsend.sh
+scp dockercheck.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
+scp outputsend.sh jenkins@$PRODUCTION_SERVER:$PRODUCTION_DIR/$PROJECT
 ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/dockercheck.sh"
 ssh jenkins@$PRODUCTION_SERVER "chmod -v u+x $PRODUCTION_DIR/$PROJECT/outputsend.sh"
 ssh jenkins@$PRODUCTION_SERVER "echo '*/5 * * * *	$PRODUCTION_DIR/$PROJECT/dockercheck.sh' > /tmp/crontab"
