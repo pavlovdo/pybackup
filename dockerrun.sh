624f167c8a71c1c157db9cefec7006dee9a153b3
Denis O. Pavlov <pavlovdo@gmail.com>
2017-01-23 16:20:39 +0300

    removing old container and start new container is transfered in build.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -7,7 +7,8 @@ HOST_MOUNT_DIR=/backup
 
 PROJECTCONTAINERS=`sudo docker ps -q --filter ancestor=ubuntu:$PROJECT`
 
-if [ -z $PROJECTCONTAINERS ] || [ -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage ]
+#if [ -z $PROJECTCONTAINERS ] || [ -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage ]
+if [ -z $PROJECTCONTAINERS ]
 then
 	echo 'Stopping project '$PROJECT' containers:'
 	sudo docker stop $PROJECTCONTAINERS
@@ -19,5 +20,5 @@ then
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR 'and '$CONFIG_DIR':'$CONFIG_DIR':ro'
 		sudo docker run -d -t --restart=always -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR -v $CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
 	fi
-	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
+#	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
 fi
