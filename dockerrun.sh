433a612b86efbc4e419c36bea34a77ad8ae43474
Denis O. Pavlov <pavlovdo@gmail.com>
2017-01-23 17:06:37 +0300

    deleted stopping of project containers from dockerrun.sh (stopping of project containers transferred to build.sh)

--- dockerrun.sh
+++ dockerrun.sh
@@ -7,18 +7,11 @@ HOST_MOUNT_DIR=/backup
 
 PROJECTCONTAINERS=`sudo docker ps -q --filter ancestor=ubuntu:$PROJECT`
 
-#if [ -z $PROJECTCONTAINERS ] || [ -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage ]
-if [ $PROJECTCONTAINERS ]
-then
-	echo 'Stopping project '$PROJECT' containers:'
-	sudo docker stop $PROJECTCONTAINERS
-	if [ -z $HOST_MOUNT_DIR ]
+if [ -z $HOST_MOUNT_DIR ]
 	then
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$CONFIG_DIR':'$CONFIG_DIR':ro'
 		sudo docker run -d -t --restart=always -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
 	else
 		echo -e '\nRunning of container from image ubuntu:'$PROJECT' with mounting '$PRODUCTION_DIR'/'$PROJECT'/data:'$PRODUCTION_DIR'/'$PROJECT'/data and '$HOST_MOUNT_DIR':'$HOST_MOUNT_DIR 'and '$CONFIG_DIR':'$CONFIG_DIR':ro'
 		sudo docker run -d -t --restart=always -v $PRODUCTION_DIR/$PROJECT/data:$PRODUCTION_DIR/$PROJECT/data -v $HOST_MOUNT_DIR:$HOST_MOUNT_DIR -v $CONFIG_DIR:$CONFIG_DIR:ro ubuntu:$PROJECT
-	fi
-#	rm -f $PRODUCTION_DIR/$PROJECT/$PROJECT.docker.newimage
 fi
